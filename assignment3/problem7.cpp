72a6576b4849396a638f04a26ae69b62e086dde9
Edelis <edelitamr@gmail.com>
2022-11-05 18:33:38 -0400

    ex 7

---
+++ assignment3/problem7.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+
+void countLines();
+
+int main()
+{
+    countLines();
+    return 0;
+}
+
+void countLines()
+{
+    ifstream fin;
+    fin.open("STORY1.TXT");
+
+    string line;
+    int count = 0;
+
+    while (!fin.eof())
+    {
+        getline(fin, line);
+        if (line[0] != 'A')
+        {
+            count++;
+        }
+    }
+
+    cout << "Number of lines not starting with A are -> " << count << endl;
+}
