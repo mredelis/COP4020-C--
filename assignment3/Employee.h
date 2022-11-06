ea4f73df006a088a8f0f01237c3ea1978603c8fd
Edelis <edelitamr@gmail.com>
2022-11-06 10:20:17 -0500

    ex 10

---
+++ assignment3/Employee.h
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+class Employee
+{
+    int ENO;
+    char ENAME[10];
+
+public:
+    // Employee(int eno, char* name)
+    // {
+    //     this->ENO = eno;
+    //     strcpy(this->ENAME, name);
+    // }
+
+    void GETIT()
+    {
+        cout << "Enter employee name: ";
+        // replace gets for getline due to compiler issues
+        // gets(ENAME);
+        cin.getline(ENAME, 10);
+        cout << "Enter employee number: ";
+        cin >> ENO;
+    }
+
+    void SHOWIT()
+    {
+        cout << ENO << ENAME << endl;
+    }
+};
---
+++ assignment3/problem10.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include <iostream>
+#include "Employee.h"
+
+using namespace std;
+
+int main()
+{
+    fstream myFile("employee.bin", ios::out | ios::in | ios::binary);
+    if (!myFile)
+    {
+        cout << "File could not be opened" << endl;
+        return 1;
+    }
+
+    Employee employee1, employee2;
+
+    // write employee object to bin file
+    employee1.GETIT();
+    myFile.write((char *)&employee1, sizeof(Employee));
+
+    // reposition put pointer to start of the file to read Employee object
+    myFile.seekp(0);
+    myFile.read((char *)&employee2, sizeof(Employee));
+    employee2.SHOWIT();
+
+    myFile.close();
+
+    return 0;
+}
