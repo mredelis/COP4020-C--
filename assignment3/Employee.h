d1dfaf2734f713e14745a76162d896fbc8ef03ad
Sara Betancourth <sarabet93@gmail.com>
2022-11-06 13:53:10 -0500

    problem 14 complete.

--- assignment3/Employee.h
+++ assignment3/Employee.h
@@ -1,30 +1,63 @@
 #include <iostream>
 using namespace std;
 
+// class Employee
+// {
+//     int ENO;
+//     char ENAME[10];
+
+// public:
+//     // Employee(int eno, char* name)
+//     // {
+//     //     this->ENO = eno;
+//     //     strcpy(this->ENAME, name);
+//     // }
+
+//     void GETIT()
+//     {
+//         cout << "Enter employee name: ";
+//         // replace gets for getline due to compiler issues
+//         // gets(ENAME);
+//         cin.getline(ENAME, 10);
+//         cout << "Enter employee number: ";
+//         cin >> ENO;
+//     }
+
+//     void SHOWIT()
+//     {
+//         cout << ENO << ENAME << endl;
+//     }
+// };
+
 class Employee
 {
-    int ENO;
-    char ENAME[10];
+    int Eno;
+    char Ename[20];
 
 public:
-    // Employee(int eno, char* name)
-    // {
-    //     this->ENO = eno;
-    //     strcpy(this->ENAME, name);
-    // }
-
-    void GETIT()
+    void getdata()
     {
-        cout << "Enter employee name: ";
-        // replace gets for getline due to compiler issues
-        // gets(ENAME);
-        cin.getline(ENAME, 10);
-        cout << "Enter employee number: ";
-        cin >> ENO;
+        cout << "Enter Employee Number: ";
+        cin >> Eno;
+        cout << "Enter Employee Name: ";
+        cin >> Ename;
     }
-
-    void SHOWIT()
+    void putdata()
     {
-        cout << ENO << ENAME << endl;
+        cout << "Employee Number: " << Eno << endl;
+        cout << "Employee Name: " << Ename << endl;
     }
+    // function to count the total number of records
+    int countRecords();
 };
+
+int Employee::countRecords()
+{
+    fstream file;
+    file.open("EMP.DAT", ios::binary | ios::in);
+    file.seekg(0, ios::end);
+    int bytes = file.tellg();
+    int count = bytes / sizeof(Employee);
+    file.close();
+    return count;
+}
---
+++ assignment3/problem14.cpp
@@ -0,0 +1,48 @@
+#include <fstream>
+#include <iostream>
+#include "Employee.h"
+using namespace std;
+
+// NOTE: to view just the amount of records in the file, comment out all lines excet lines: 32 & 40
+
+int main()
+{
+    // create a file object
+    fstream file("EMP.DAT", ios::out | ios::in | ios::binary);
+
+    // if file does not exist, create it
+    if (!file)
+    {
+        cout << "File could not be opened" << endl;
+        return 1;
+    }
+
+    // First, write Employee objects to file
+    Employee employee; // create an Employee object
+    int count;         // count of records
+
+    cout << "Enter number of employee records to write: ";
+    cin >> count; // get the number of records to write
+
+    while (count > 0)
+    {
+        employee.getdata();
+        file.write(reinterpret_cast<char *>(&employee), sizeof(Employee));
+        count--;
+    }
+
+    // Second, read objects from file and display them
+    Employee inEmployee;
+    file.seekp(0); // move to the beginning of the file
+
+    while (file.peek() != EOF)
+    {
+        file.read(reinterpret_cast<char *>(&inEmployee), sizeof(Employee)); // read the next record
+        inEmployee.putdata();                                               // display the record
+    }
+
+    // Third, count the number of records in the file
+    cout << "Total number of Employee records: " << inEmployee.countRecords() << endl;
+
+    file.close();
+}
