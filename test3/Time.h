7f9f28be31cb25e93e2d34692374b3037794e321
Sara Betancourth <sarabet93@gmail.com>
2022-11-10 14:09:47 -0500

    problem 4 complete

---
+++ test3/Time.h
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+class Time
+{
+private:
+    int hours, minutes;
+
+public:
+    // sets the hours and minutes of the time object
+    void settime(int hours, int minutes)
+    {
+        this->hours = hours;
+        this->minutes = minutes;
+    }
+
+    // displays the time in hours and minutes
+    void showtime()
+    {
+        cout << hours << " hours " << minutes << " minutes" << endl;
+    }
+
+    // returns the sum of two time objects
+    Time sum(Time t)
+    {
+        Time temp;
+        temp.hours = hours + t.hours;
+        temp.minutes = minutes + t.minutes;
+
+        // convert minutes to hours if >= 60
+        while (temp.minutes >= 60)
+        {
+            temp.minutes -= 60;
+            temp.hours++;
+        }
+
+        return temp;
+    }
+};
---
+++ test3/problem4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Time.h"
+
+using namespace std;
+
+int main()
+{
+    // Part b.
+    // creates three time objects
+    Time t1, t2, t3;
+
+    // sets the hours and minutes of the first two times
+    t1.settime(11, 60);
+    t2.settime(15, 45);
+
+    // calls the sum function to add the first two times
+    // and assigns the result to the third time
+    t3 = t1.sum(t2);
+
+    // displays all times
+    cout << "Time 1:" << endl;
+    t1.showtime();
+    cout << endl;
+    cout << "Time 2:" << endl;
+    t2.showtime();
+    cout << endl;
+    cout << "Time 3:" << endl;
+    t3.showtime();
+
+    return 0;
+}
